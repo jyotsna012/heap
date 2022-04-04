af73aff42bede8ead128ebf60cf295c0b6ea4a71
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-04-04 11:43:32 -0700

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -11,7 +11,6 @@ int main(){
 
 int numelements = 0;
 int* heap = new int[101];
-int* heapSecond = new int[101][2];
 cout << "enter the number of elements you are inputting" << endl;
 cin >> numelements;
 cin.get();
@@ -29,40 +28,9 @@ int toStart = numelements/2;
 for(int a = toStart; a >=1; a--){
   heapify(heap, numelements, a);
 }
-for(int c = 1; c <= numelements; c++){
-            if(i <= 1){
-                   heapSecond[c][0] = heap[c];
-                   heapSecond[c][1] = 1;  
-            }
-            else if(i > 1 && i <= 3){
-                   heapSecond[c][0] = heap[c];
-                   heapSecond[c][1] = 2;
-            }
-            else if(i > 3 && i <= 7){
-                   heapSecond[c][0] = heap[c];
-                   heapSecond[c][1] = 3;
-            }
-            else if(i > 7 && i <= 15){
-              heapSecond[c][0] = heap[c];
-                  heapSecond[c][1] = 4;    
-            }
-            else if(i > 15 && i <= 31){
-                  heapSecond[c][0] = heap[c];
-                  heapSecond[c][1] = 5;
-            }
-            else if(i > 31 && i <= 63){
-                  heapSecond[c][0] = heap[c];
-                  heapSecond[c][1] = 6;
-            }
-            else if(i > 63 && i <= 127){
-                  heapSecond[c][0] = heap[c];
-                  heapSecond[c][1] = 7;
-            }
-}
-      
 
 printHeap(heap, numelements);   
-visualHeap(heap, numelements, heapSecond);
+visualHeap(heap, numelements);
 
 for(int b = numelements; b >= 1; b--){
     int temp = heap[1];
@@ -113,7 +81,7 @@ void printHeap(int* &arr, int n)
  
 }
 
-void visualHeap(int* &heap, int numelements, int* &heapSecond){
+void visualHeap(int* &heap, int numelements){
       for(int i = 1; i <= numelements; i++){
             if(i <= 1){
                   cout << "" << heap[i] << endl;
