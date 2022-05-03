93e00b1a2e06c9edb809235d26ab5f474041f78a
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-05-02 22:36:54 -0700

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -37,9 +37,9 @@ for(int a = toStart; a >=1; a--){
 }
 cout << "ori" <<endl;       
 printHeap(heap, numelements);   
+      
 //visualHeap(heap, numelements);
-
-int numelements2 = numelements;
+/*int numelements2 = numelements;
 for(int b = numelements2; b >= 1; b--){
     int temp = heap[1];
     heap[1] = heap[b];
@@ -50,22 +50,18 @@ for(int b = numelements2; b >= 1; b--){
     heapify(heap, numelements2, numelements2/2);
     printHeap(heap, numelements);   
 
-}
+}*/
+  heapSort(heap, numelements);
 
-  printOrderedHeap(orderedheap, numelements); 
+  //printOrderedHeap(orderedheap, numelements); 
     
 }
 
 void heapify(int* &arr, int n, int i){
   
-  //cout << "Heap Start" << endl;
   int largest = i;
- // cout << "i value: " << arr[largest] << endl;
   int left = 2 * i ;
- // cout << "left: " << arr[left] << endl;
   int right = 2 * i + 1;
-  //cout << "right: " << arr[right] << endl;
-
   
   while(left <= n && arr[left] > arr[largest]){
     largest = left; 
@@ -76,16 +72,11 @@ void heapify(int* &arr, int n, int i){
   }
   
   if(largest != i){
-    //cout << "largest index: " << largest << endl;
-    //cout << "input: index " << i << endl;
-    //cout << "heap swaped(i, largest): " << arr[i] << "and" << arr[largest] << endl;
     int temp = arr[i];
     arr[i] = arr[largest];
     arr[largest] = temp;
     heapify(arr, n, largest);
   }
-      
-  //cout << "Heap End" << endl;
 }
 
 void printHeap(int* &arr, int n)
