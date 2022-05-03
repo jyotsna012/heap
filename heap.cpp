d6615cdad520a3fcae908a3f6f4335012a765f50
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-05-02 22:45:46 -0700

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -132,18 +132,4 @@ void visualHeap(int* &heap, int numelements){
       }
 }
 
-void heapSort(int* &arr, int n)
-{
-    // Build heap (rearrange array)
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
- 
-    // One by one extract an element from heap
-    for (int i = n - 1; i >= 0; i--) {
-        // Move current root to end
-        swap(arr[0], arr[i]);
- 
-        // call min heapify on the reduced heap
-        heapify(arr, i, 0);
-    }
-}
+
